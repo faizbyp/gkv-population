ac52e07893c1db399de46bb7b4cf378c23bf0d7b
faizbyp <faizbyp@gmail.com>
2022-06-03 11:28:16 +0700

    menyesuaikan pie chart

--- piechart.js
+++ piechart.js
@@ -1,17 +1,18 @@
 var cpie = document.getElementById("pie");
 var ctxpie = cpie.getContext("2d");
 
-var width = 600;
-var height = 400;
-var x = 200;
-var y = 200;
-var r = 150;
+var width = 1000;
+var height = 720;
+var x = 400;
+var y = 360;
+var r = 300;
+var rectW = 50;
 var startDeg = 0-(0.5*Math.PI);
 
 var negara=[];
 var populasi=[];
 
-d3.csv("pops_sampled.csv",function(csv){
+d3.csv("pops_sorted.csv",function(csv){
   csv.map(function(d){
     negara.push(d.Country);
     populasi.push(+d.Population);
@@ -34,23 +35,31 @@ d3.csv("pops_sampled.csv",function(csv){
   for(var i=0; i<n; i++){
     var randomColor = '#' + Math.random().toString(16).slice(2, 8);
     
-    ctxpie.fillStyle = palette1[i];
+    // Draw slices of pie
+    if(i === 0){ ctxpie.fillStyle = palette1[i]; }  
+    else if(i > 0){ ctxpie.fillStyle = palette1[2]; }
     ctxpie.beginPath();
     ctxpie.moveTo(x,y);
-    ctxpie.arc(x, y, r, startDeg, startDeg+(populasi[i]/total)*(2*Math.PI));
+    ctxpie.arc(x, y, r, startDeg, startDeg-(populasi[i]/total)*(2*Math.PI), true);
     ctxpie.closePath();
     ctxpie.fill();
 
-    ctxpie.fillRect(width-50, 50+(i*40), 30, 30);
-    ctxpie.strokeStyle = "black";
-    ctxpie.strokeRect(width-50, 50+(i*40), 30, 30);
-    ctxpie.fillStyle = 'black';
-    ctxpie.textAlign = 'end';
-    ctxpie.textBaseline = 'middle';
-    ctxpie.fillText(negara[i], width-60, 65+(i*40));
+    if(i < 2){
+      ctxpie.font = "24px Plus Jakarta Sans";
+      ctxpie.fillRect(width-150, 70+(i*60), rectW, rectW);
+      ctxpie.fillStyle = 'black';
+      ctxpie.textAlign = 'end';
+      ctxpie.textBaseline = 'middle';
+      if(i === 0){
+        ctxpie.fillText(negara[i], width-170, 95+(i*60));
+      }
+      else if(i === 1){
+        ctxpie.fillText('Others', width-170, 95+(i*60));
+      }
+    }
 
     console.log(startDeg);
-    startDeg += (populasi[i]/total)*(2*Math.PI);
+    startDeg -= (populasi[i]/total)*(2*Math.PI);
 
   }
 });
